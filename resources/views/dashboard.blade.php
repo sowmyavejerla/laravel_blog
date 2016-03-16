7164c48941e90cd351641301cdd092828e239f47
sowmyavejerla <sowmya@innovateapps.com>
2016-03-16 12:59:26 +0530

    added dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,2 +1,12 @@
 @extends('layout') @section('content')
+
+ @if(Session::has('name'))
+    {{ Session::get('name') }}
+    @else
+         {{ Session::get('name') }}
+                @endif
+
+
+dfg
+
 @endsection
